1dede57e3f7a51384c2c8bef83ecb1756a0c0f86
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2025-01-08 18:56:21 +0100

    Implement Sentry

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
-import Navbar from "./components/Navbar";
+
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
-    <>
-      <main>
-        <Navbar />
-        <Hero />
-        <Highlights />
-        <Model />
-      </main>
-    </>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
